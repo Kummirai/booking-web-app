603dc4ff016f1cf2aad18388244f2c090d9c008f
Milton <ajaxmilton@hotmail.com>
2026-03-13 00:26:21 +0200

    feat(layout): add site footer component
    - Create reusable Footer component
    - Add responsive design for mobile/tablet/desktop
    - Include copyright and navigation links

--- components/blocks/Card.jsx
+++ components/blocks/Card.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import Image from "next/image";
 import { LuSquareArrowOutUpRight } from "react-icons/lu";
-import { FaStar } from "react-icons/fa6";
-import { MdPool } from "react-icons/md";
-import { FaDog } from "react-icons/fa";
-import { FaChildren } from "react-icons/fa6";
-import { MdNoMealsOuline } from "react-icons/md";
-import { MdGolfCourse } from "react-icons/md";
-import { FaSwimmingPool } from "react-icons/fa";
-import { MdFreeBreakfast } from "react-icons/md";
-import { TbAirConditioning } from "react-icons/tb";
-import { FaHotTub } from "react-icons/fa";
-import { CiStar } from "react-icons/ci";
-import { FaStarHalfAlt } from "react-icons/fa";
+
 
 export default function Card({ hotel }) {
-  console.log(hotel);
-  const amenityIcons = [
-    { id: 1, amenity: "Pool", icon: <MdPool /> },
-    { id: 2, amenity: "Pet-friendly", icon: <FaDog /> },
-    { id: 3, amenity: "Kid-friendly", icon: <FaChildren /> },
-    { id: 4, amenity: "Restaurant", icon: <MdNoMealsOuline /> },
-    { id: 5, amenity: "Golf", icon: <MdGolfCourse /> },
-    { id: 6, amenity: "Outdoor pool", icon: <FaSwimmingPool /> },
-    { id: 7, amenity: "Free breakfast", icon: <MdFreeBreakfast /> },
-    { id: 8, amenity: "Air conditioning", icon: <TbAirConditioning /> },
-    { id: 9, amenity: "Hot tub", icon: <FaHotTub /> },
-  ];
+  const createRating = (rating) => {
+    const remainder = rating % 1;
+    const whole = Math.trunc(rating);
+    const fullStar = "★";
+    const halfFullStar = "⯨";
+    const emptyStar = "☆";
+
+    let stars = "";
+
+    for (let i = 0; i < whole; i++) {
+      stars += fullStar;
+    }
+
+    if (remainder) {
+      stars += halfFullStar;
+    }
+    console.log(stars);
+    return stars;
+  };
 
   return (
     <div className="h-115 flex flex-col">
@@ -50,9 +46,8 @@ export default function Card({ hotel }) {
               <span className="text-sm text-gray-500">/per night</span>
             </p>
             <p className="flex my-1 items-center">
-              <span className="mr-1">
-                <FaStar className="text-yellow-300" />
-                <CiStar />
+              <span className="mr-1 flex items-center text-yellow-400 text-md">
+                {createRating(hotel.overall_rating)}
               </span>
               <span className="text-sm text-gray-600">
                 {hotel.overall_rating}
@@ -65,28 +60,7 @@ export default function Card({ hotel }) {
             </p>
             <p>{hotel.description}</p>
           </div>
-          {/* <div className="grid grid-cols-2 gap-2 py-4 mt-5 border-t border-t-gray-100 rounded-b-2xl text-gray-500 ">
-            {hotel.amenities?.map((amenity, index) => {
-              return (
-                <div key={amenity}>
-                  <p className="flex items-center ">
-                    {amenityIcons.map((amenityIcon, index) => {
-                      amenity === amenityIcon.amenity ? (
-                        <span key={index} className="mr-1 ">
-                          {amenityIcon.icon}
-                          here
-                        </span>
-                      ) : (
-                        <FaHotTub />
-                      );
-                    })}
 
-                    <span className="font-sans line-clamp-1">{amenity}</span>
-                  </p>
-                </div>
-              );
-            })}
-          </div> */}
           <div className="absolute bottom-4">
             <button className="flex items-center py-1 px-5  text-white rounded-sm relative bg-linear-to-r from-purple-700 to-[#764de1] hover:cursor-pointer">
               <span className="mr-1">More details</span>
---
+++ components/blocks/Details.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaStar } from "react-icons/fa6";
+import { MdPool } from "react-icons/md";
+import { FaDog } from "react-icons/fa";
+import { FaChildren } from "react-icons/fa6";
+import { MdNoMealsOuline } from "react-icons/md";
+import { MdGolfCourse } from "react-icons/md";
+import { FaSwimmingPool } from "react-icons/fa";
+import { MdFreeBreakfast } from "react-icons/md";
+import { TbAirConditioning } from "react-icons/tb";
+import { FaHotTub } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
+import { FaStarHalfAlt } from "react-icons/fa";
+export default function Details() {
+  return <div>Details</div>;
+}
